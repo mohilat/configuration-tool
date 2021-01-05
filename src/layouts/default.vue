626d3eba305197567d18fc789acf00921538cbd7
Stepan Mohilat <stepan.mohilat@globallogic.com>
2021-01-05 18:50:12 +0100

    add

---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+        <v-app-bar
+            app
+            dark
+            dense
+            absolute
+            color="black"
+        >
+        </v-app-bar>
+
+        <v-content>
+            <nuxt/>
+        </v-content>
+    </v-app>
+</template>
